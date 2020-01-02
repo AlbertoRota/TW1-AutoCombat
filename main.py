dfd47c2114e78aa7d97a89dda13d34ab02704037
Alberto Rota <alberto.rota.jimenez@gmail.com>
2020-01-02 22:19:02 +0100

    Added sound and "Change mode" logic.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Standard library imports
+import winsound
 from time import sleep
 
 # Third party imports
@@ -7,27 +8,67 @@ import pyautogui
 # Local application imports
 from main.code.windowMgr import WindowMgr
 from main.code.imageMatcher import ImageMatcher
-from main.code.debugUtils import DebugUtils
+from pynput import keyboard
+
+# Global variables
+end_program = False
+key_pressed = False
+mode = 2
+
+
+def on_press(key):
+    global end_program, key_pressed
+
+    if key == keyboard.Key.esc:
+        end_program = True  # Flip global flag
+        return False  # Stop listener
+    elif key == keyboard.Key.tab:
+        key_pressed = True  # Flip global flag
 
 
 def main():
-    # Initialize all modules.
-    w = WindowMgr("The Witcher.*")
+    global end_program, key_pressed, mode
+
+    # Initialize keyboard listener on a separate thread
+    keyboard.Listener(on_press=on_press).start()
+
+    # Initialize all the internal modules.
+    w_mgr = WindowMgr("The Witcher.*")
     im = ImageMatcher(path_to_resources='main/resources/templates')
-    d = DebugUtils(True)
 
     # Main loop
-    while True:
-        # Get a fresh image
-        img = w.get_mouse_area()
+    mode_audios = [
+        'main/resources/sounds/combatMode.wav',
+        'main/resources/sounds/lootMode.wav',
+        'main/resources/sounds/sleepMode.wav'
+    ]
+    winsound.PlaySound('main/resources/sounds/bootUp.wav', winsound.SND_FILENAME)
+    while not end_program:
+        # Change mode if needed
+        if key_pressed:
+            mode = 0 if mode == 2 else mode + 1
+            winsound.PlaySound(mode_audios[mode], winsound.SND_FILENAME | winsound.SND_ASYNC)
+            key_pressed = False
 
         # Main decision tree
-        if im.is_combat_combo(img):
-            pyautogui.click(pyautogui.position())
-            sleep(1)
-        elif im.is_start_combat(img):
-            pyautogui.click(pyautogui.position())
-            sleep(1)
+        if mode == 0:
+            # Combat mode
+            img = w_mgr.get_mouse_area()
+            if im.is_combat_combo(img) or im.is_start_combat(img):
+                pyautogui.click(pyautogui.position())
+                sleep(1)
+        elif mode == 1:
+            # Gather mode
+            img = w_mgr.get_mouse_area()
+            if im.is_gather(img):
+                pyautogui.click(pyautogui.position())
+                sleep(1)
+        elif mode == 2:
+            # Sleep mode
+            sleep(0.5)
+
+    # Play "Shutting down" sound
+    winsound.PlaySound('main/resources/sounds/shutDown.wav', winsound.SND_FILENAME)
 
 
 if __name__ == '__main__':
